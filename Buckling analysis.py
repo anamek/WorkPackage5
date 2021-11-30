a140d69235fc358df9461867488293ffbc3a0aaf
Jellevdm <j.h.c.vandermeer@gmail.com>
2021-11-30 14:51:05 +0100

    added functions partialy

--- Buckling analysis.py
+++ Buckling analysis.py
@@ -1,6 +1,22 @@
 import math
-print("test")
+
+#----------------------------------------------Constant values and list of properties-----------------------------------
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 
 
+#----------------------------------------------Function definition for buckling analysis--------------------------------
+
+def euler_column_buckling (E, I, A, L):
+    sigma_cr = (math.pi ** 2 * E * I) / (A * L ** 2)
+    return sigma_cr
+
+def shell_buckling_k (Lambda, L, R, t1, poisson):
+    k = Lambda + (12) / (math.pi ** 4) * (L ** 4) / (R ** 2 * t1 ** 2)  * (1 - poisson ** 2) * 1 / Lambda
+    return k
+
+def shell_buckling_Q ():
+
+def shell_buckling ():
 
