705b66585592b9b74c6f4d9da2ceaf3e39bc3919
Alan <93991836+Vimlopop@users.noreply.github.com>
2021-11-30 14:57:48 +0100

    Magnitude of forces

---
+++ AttachmentMass.py
@@ -0,0 +1,9 @@
+import numpy as np
+# WP 4 forces; [500, 7306.3, 500] F_1 = [1924.7, 1924.7, 1988.9]
+
+f_1 = [1924.7, 1924.7, 1988.9]
+f_0 = [500, 7306.3, 500]
+f_t = np.add(f_0,f_1)
+magnitude = np.linalg.norm(f_t)
+
+print(round(magnitude,3))
