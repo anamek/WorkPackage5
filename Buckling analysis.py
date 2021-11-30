ed48154a159343d40c60feb47b9a49ec37430cc3
Jellevdm <j.h.c.vandermeer@gmail.com>
2021-11-30 15:16:13 +0100

    ...

--- Buckling analysis.py
+++ Buckling analysis.py
@@ -16,7 +16,26 @@ def shell_buckling_k (Lambda, L, R, t1, poisson):
     k = Lambda + (12) / (math.pi ** 4) * (L ** 4) / (R ** 2 * t1 ** 2)  * (1 - poisson ** 2) * 1 / Lambda
     return k
 
-def shell_buckling_Q ():
+def shell_buckling_Q (p, E, R, t1):
+    Q = ((p) / (E)) * ((R) / (t1)) ** 2
+    return Q
 
-def shell_buckling ():
+def shell_buckling (Q, k, E, poisson, t1, L):
+    sigma_cr = (1.983 - 0.983 * math.exp(-23.14 * Q)) * k * ((math.pi ** 2 * E) / (12 * (1 - poisson ** 2))) * (t1 / L) ** 2
+    return sigma_cr
 
+#----------------------------------------------------------Check for buckling failure-----------------------------------
+
+applied_stress = 0#yet to connect and determine
+result = [] #yet to be filled in
+
+print(result[0], result[1])
+
+for i in result:
+    if i > applied_stress:
+        print("The applied stress is lower the critical stress, therefore this configuration is SAFE")
+    elif i < applied_stress:
+        print("The applied stress is higher then the critical stress, therefore this configuration is NOT SAFE")
+#-----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------WP2 configuration check------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
