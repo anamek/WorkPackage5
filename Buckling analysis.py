ed3edda7aa7511fee2c62d4369cae32f8ff684ee
Jellevdm <j.h.c.vandermeer@gmail.com>
2021-12-02 14:31:43 +0100

    ...

--- Buckling analysis.py
+++ Buckling analysis.py
@@ -2,7 +2,7 @@ import math
 from basicparameters import *
 from MaterialSelection import *
 #----------------------------------------------Function definition for buckling analysis--------------------------------
-
+L = h
 def euler_column_buckling (E, I, A, L):
     sigma_cr = (math.pi ** 2 * E * I) / (A * L ** 2)
     return sigma_cr
@@ -11,8 +11,8 @@ def shell_buckling_k (Lambda, L, R, t1, poisson):
     k = Lambda + (12) / (math.pi ** 4) * (L ** 4) / (R ** 2 * t1 ** 2)  * (1 - poisson ** 2) * 1 / Lambda
     return k
 
-def shell_buckling_Q (p, E, R, t1):
-    Q = ((p) / (E)) * ((R) / (t1)) ** 2
+def shell_buckling_Q (P, E, R, t1):
+    Q = ((P) / (E)) * ((R) / (t1)) ** 2
     return Q
 
 def shell_buckling (Q, k, E, poisson, t1, L):
@@ -20,19 +20,18 @@ def shell_buckling (Q, k, E, poisson, t1, L):
     return sigma_cr
 
 #----------------------------------------------------------Check for buckling failure-----------------------------------
-sigma_cr_e = euler_column_buckling(E, I, A, 1) # length still needs to be linked
+sigma_cr_e = euler_column_buckling(E, I, A, L) # [MPa]
 
-shell_buckling_k(0.5, 1, R, t_1, poisson)
-
-
-
-
+k = shell_buckling_k(0.5, L, R, t_1, poisson)
+Q = shell_buckling_Q(P, E, R, t_1)
+sigma_cr_s = shell_buckling(Q, k, E, poisson, t_1, L) /1000000  #[MPa]
 
 
 applied_stress = 0#yet to connect and determine
-result = [] #yet to be filled in
+result = [sigma_cr_e, sigma_cr_s] #yet to be filled in
 
-print(result[0], result[1])
+
+print("Column Buckling:", result[0], "Shell Buckling:", result[1])
 
 for i in result:
     if i > applied_stress:
--- MaterialSelection.py
+++ MaterialSelection.py
@@ -1,11 +1,11 @@
 R=1.1       #in meters
 P=1950000 #in Pa, from WP2
-h=1.2        #arbitrary
+h= 1.2        #arbitrary
 rho=4520    #aluminium (kg/m3)
 SF=1.375 #from ADSEE
 sigma_y=880000000 #Pa, aluminum 2024 T3
-E = 731. * 10 ** 9 #Pa, aluminim 2024 T3
-poisson =
+E = 73.1 * 10 ** 9 #Pa, aluminim 2024 T3
+poisson = 0.33  #v, aluminium 2024 T3
 def tank_internal_stresses(pressure, radius, yield_strength,safety_factor):
     thickness_cylinder= (pressure*radius)/yield_strength*safety_factor
     thickness_sphere=(pressure*radius)/(2*yield_strength)*safety_factor
@@ -23,7 +23,7 @@ def mass_calculation(radius,thickness_cylinder, thickness_sphere, height,density
     mass_total=mass_cylinder+mass_spherical
     return mass_total
 
-mass=mass_calculation(R,t_1,t_2,h,rho)
+mass = mass_calculation(R,t_1,t_2,h,rho)
 
 
 print("Mass of the tank: ",mass)
